8e96d28559cd21c9c10432cf9b2ff98989c4caae
Gérald Barbosa <barbosa.gerald@gmail.com>
2019-11-12 16:00:46 +0100

    Counter and input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
-  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  // Déclare une fonction setCount qui permet de modifier la valeur de count 
   const [count, setCount] = useState(0);
 
   return (
